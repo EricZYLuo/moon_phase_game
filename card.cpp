a11930d3e5ace7b94aa425db79b528cbd325bb6a
Eric Luo <ericzyluo2002@gmail.com>
2024-12-10 17:07:25 -0500

    Implemented Card and Deck member functions (not tested)

--- card.cpp
+++ card.cpp
@@ -1,1 +1,48 @@
+#include <algorithm>
+#include <chrono>
+#include <random>
 
+#include "card.h"
+
+Card::Card(): id{-1}, phase{new_moon} {}
+
+Card::Card(int id, Phases phase): id{id}, phase{phase} {}
+
+Deck::Deck(): id{-1} {
+    // Default deck size of 4 copies of each
+    for(int i = 0; i < 4; ++i) {
+        for(int j = 0; j < 8; ++j) {
+            deck.emplace(Card(i*8+j, (Phases) j));
+        }
+    }
+}
+
+Deck::Deck(int id, int copies): id{id} {
+    for(int i = 0; i < copies; ++i) {
+        for(int j = 0; j < 8; ++j) {
+            deck.emplace(Card(i*8+j, (Phases) j));
+        }
+    }
+}
+
+void Deck::shuffle() {
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+
+    shuffle(this->deck.front(), this->deck.back(), std::default_random_engine(seed));
+
+}
+
+Card* Deck::deal() {
+    Card* top = this->deck.front();
+    this->deck.pop();
+    return top;
+}
+
+Deck::~Deck() {
+    Card* top;
+    while(!this->deck.empty()) {
+        top = this->deck.front();
+        this->deck.pop();
+        delete top;
+    }
+}
--- card.h
+++ card.h
@@ -1,15 +1,29 @@
 #include <string>
+#include <queue>
 
-enum Phases {new_moon, waxing_crescent, first_quarter, waxing_gibbeous, full_moon, waning_gibbeous, last_quarter, waning_crescent};
+enum Phases {new_moon = 0, waxing_crescent = 1, first_quarter = 2, waxing_gibbeous = 3, full_moon = 4, waning_gibbeous = 5, last_quarter = 6, waning_crescent = 7};
 
 class Card {
-
+    int id;
     Phases phase;
+
+public:
+    Card();
+    Card(int id, Phases phase);
+
 
 };
 
 class Deck {
+    int id;
+    std::queue<Card*> deck;
 
-    Card* stack;
+    void shuffle();
+
+public:
+    Deck();
+    Deck(int id, int copies);
+    Card* deal();
+    ~Deck();
 
 };
